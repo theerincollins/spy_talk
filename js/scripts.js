0913e3c188da19942c1c4b2cb856530981f83f53
The Erin Collins & Peter <student@epicodus.com>
2015-05-26 13:49:58 -0700

    gives the number of rows

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,31 @@
+
+
 var spyLanguage = function(string) {
   return string.toLowerCase().replace(/[^a-z0-9]/g, "");
-
 };
+
+var columns = function(inputString){
+  var resultString = inputString.length;
+  return Math.ceil(Math.sqrt(resultString));
+};
+
+var rows = function(inputString) {
+  return Math.ceil(inputString.length/columns(inputString));
+};
+
+
+
+
+
+
+
+
+
+
+//   var result-string = inputString.length;
+//   var number-of-columns = 5;
+//   var number-of-rows = Math.ceil(9);
+//
+//   return number-of-rows;
+//   return Math.floor(Math.sqrt(resultString));
+// };
--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,28 @@
 describe('spyLanguage', function(){
-  it('downcases all letters in a string', function(){
-      expect(spyLanguage("Peter is a Cool-guy!")).to.equal("peterisacoolguy")
+  it('downcases all letters in a string', function() {
+    expect(spyLanguage("Peter is a Cool-guy!")).to.equal("peterisacoolguy")
   });
-  it('removes punctuation from sting', function(){
-      expect(spyLanguage("Erin is a Damn-COOL-CHick")).to.equal("erinisadamncoolchick")
 
+  it('removes punctuation from sting', function() {
+    expect(spyLanguage("Erin is a Damn-COOL-CHick")).to.equal("erinisadamncoolchick")
+  });
+
+});
+
+describe('columns', function(){
+  it('calculates the number of columns', function() {
+    expect(columns(spyLanguage("Erin is a Damn-COOL-CHick"))).to.equal(5)
+  });
+});
+
+describe('rows', function(){
+  it("calculates the number of rows", function() {
+    expect(rows(spyLanguage("Erin is a Damn-COOL-CHick"))).to.equal(4)
+  });
+});
+
+describe('split', function(){
+  it("will split the string into multiple string at a calculated length of columns", function(){
+    expect(split("Break this")).to.equal(["bre", "akt", "his"])
   });
 });
