5e57186f33d7a935d783bf4d03c129bc2149459a
The Erin Collins & Peter <student@epicodus.com>
2015-05-26 16:40:33 -0700

    still working on the last part

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,63 @@ var spyLanguage = function(string) {
 };
 
 var columns = function(inputString){
-  var resultString = inputString.length;
-  return Math.ceil(Math.sqrt(resultString));
+  var resultLength = spyLanguage(inputString).length;
+  return Math.ceil(Math.sqrt(resultLength));
 };
 
 var rows = function(inputString) {
-  return Math.ceil(inputString.length/columns(inputString));
+  var resultLength = spyLanguage(inputString).length;
+  return Math.ceil(resultLength/columns(inputString));
 };
+
+var sortLetters = function(inputString) {
+  var returnArray = [];
+  var sliceString = spyLanguage(inputString);
+  var numberColumns = columns(inputString);
+  var length = spyLanguage(inputString).length;
+  var numberRows = rows(inputString);
+
+  var first = 0;
+  var last = numberColumns
+
+  for (var index = 1 ; index <= numberRows; index++) {
+    var n = sliceString.slice(first, last);
+    returnArray.push(n);
+    first = last;
+    last = last + (numberColumns);
+  }
+  return returnArray;
+};
+
+
+var finalSpy = function(inputString) {
+  var firstArray = sortLetters(inputString);
+  var array1 = [];
+  var lettersArray;
+  var finalArray = [];
+  var n;
+  firstArray.forEach(function(letters) {
+    lettersArray = letters.split("");
+    array1.push(lettersArray);
+  });
+
+  for(var index = 0; index <= array1.length-1; index++)
+    array1.forEach(function(letters){
+      finalArray.push(letters[index])
+    });
+debugger;
+    return finalArray.join("").slice(0,4);
+
+
+};
+
+// var splits = sortLetters(inputString);
+//
+
+//
+// return splits;
+
+
+// var n = spyLanguage(sliceString.slice(0, numberColumns-1));
+// returnArray.push(n);
+// sliceString = spyLanguage(inputString).replace(n, "");
--- spec/specs.js
+++ spec/specs.js
@@ -11,18 +11,28 @@ describe('spyLanguage', function(){
 
 describe('columns', function(){
   it('calculates the number of columns', function() {
-    expect(columns(spyLanguage("Erin is a Damn-COOL-CHick"))).to.equal(5)
+    expect(columns("Erin is a Damn-COOL-CHick")).to.equal(5)
   });
 });
 
 describe('rows', function(){
   it("calculates the number of rows", function() {
-    expect(rows(spyLanguage("Erin is a Damn-COOL-CHick"))).to.equal(4)
+    expect(rows("Erin is a Damn-COOL-CHick")).to.equal(4)
   });
 });
 
 describe('split', function(){
   it("will split the string into multiple string at a calculated length of columns", function(){
-    expect(split("Break this")).to.equal(["bre", "akt", "his"])
+    expect(sortLetters("Break this")).to.eql(["bre", "akt", "his"])
+  });
+
+  it("will split the string into multiple string at a calculated length of columns", function(){
+    expect(sortLetters("Break thiss")).to.eql(["brea", "kthi", "ss"])
+  });
+});
+
+describe('finalSpy', function(){
+  it("will output spy code in sets of five letters", function(){
+    expect(finalSpy("Break this")).to.eql(["bahrk", "iets"])
   });
 });
