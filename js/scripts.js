0290df109e3640622b7862c1f4d48608e14d5d6a
The Erin Collins & Peter <student@epicodus.com>
2015-05-26 13:50:08 -0700

    gives the number of rows

--- js/scripts.js
+++ js/scripts.js
@@ -12,20 +12,3 @@ var columns = function(inputString){
 var rows = function(inputString) {
   return Math.ceil(inputString.length/columns(inputString));
 };
-
-
-
-
-
-
-
-
-
-
-//   var result-string = inputString.length;
-//   var number-of-columns = 5;
-//   var number-of-rows = Math.ceil(9);
-//
-//   return number-of-rows;
-//   return Math.floor(Math.sqrt(resultString));
-// };
